0258339fbdf13fe3b67d02c950f6bd0e37995da8
KartachovIA <ikartachov@yandex.ru>
2020-02-22 23:30:50 +0300

    Создаем скелет портфолио

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header/Header";
+import AboutMe from "./AboutMe/AboutMe";
+import MySkills from "./MySkills/MySkills";
+import MyProjects from "./MyProjects/MyProjects";
+import InputDates from "./InputDates/InputDates";
+import JobOffer from "./JobOffer/JobOffer";
+import SocialNets from "./SocialNets/SocialNets";
 
-class App extends React.Component {
-    render = () => {
-        return (
-            <div className="App">
 
+const App = () => {
+    return (
+        <div className="app">
+            <div className="container">
+                <Header/>
+                <AboutMe/>
+                <MySkills/>
+                <MyProjects/>
+                <InputDates/>
+                <JobOffer/>
+                <SocialNets/>
             </div>
-        );
-    }
-}
+        </div>
+    )
+};
+
 
 export default App;
 
