6e56752b568b72658d0ecac4e1f471ce097369f4
KartachovIA <ikartachov@yandex.ru>
2020-02-25 09:05:41 +0300

    26.02.2020

--- src/MyProjects/MyProjects.js
+++ src/MyProjects/MyProjects.js
@@ -4,7 +4,7 @@ import MyProject from "./MyProject";
 
 const MyProjects = () => {
     return (
-        <div className={c.section3}>
+        <div className={c.sectionProjects}>
             <div className={c.myProjects}>
                <div className={c.myProjects_header}>
                     <span>Мои работы</span>
